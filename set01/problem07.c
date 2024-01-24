0d90ab9504bbb92c3a7a669e80834ace2ea46432
unknown <shamithar.bsc23@rvu.edu.in>
2024-01-24 20:40:42 +0530

    test

---
+++ set01/problem07.c
@@ -0,0 +1,26 @@
+//Write a C program to find sum of all natural numbers until _n_
+#include<stdio.h>
+int input_n()
+{
+    int n;
+    printf("Enter number:");
+    scanf("%d",&n);
+    return n;
+}
+int sum_n_nos(int n)
+{
+    int sum=0;
+    for(int i=1;i<=n;i++)
+    {sum = sum+i;}
+    return sum;
+}
+void output(int n, int sum){
+    printf("The sum of %d numbers is %d",n,sum);
+}
+int main(){
+    int n, sum;
+    n=input_n();
+    sum=sum_n_nos(n);
+    output(n,sum);
+    return 0 ;
+}
---
+++ set01/problem08.c
@@ -0,0 +1,38 @@
+//Write a C program to find sum of _n_ different numbers entered by the user.
+#include<stdio.h>
+int input_array_size()
+{
+    int n;
+    printf("Enter the size of array:");
+    scanf("%d",&n);
+    return n;
+}
+void input_array(int n, int a[n])
+{
+    for(int i=0;i<n;i++)
+    {
+        printf("Enter %d values:",i+1);
+        scanf("%d",&a[i]);
+    }
+}
+int sum_n_array(int n, int a[n])
+{
+    int sum=0;
+    for(int i=0;i<n;i++){
+        sum=sum+a[i];
+    }
+    return sum;
+}
+void output(int n,int sum)
+{
+    printf("The sum of %d numbers is %d",n,sum);
+}
+int main()
+{
+    int n = input_array_size();
+    int a[n];
+    input_array(n,a);
+    int sum = sum_n_array(n,a);
+    output(n,sum);
+    return 0 ;
+}
