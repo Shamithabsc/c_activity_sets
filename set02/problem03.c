aac0c839956b17a8a02502959243ad6a19129fb9
unknown <shamithar.bsc23@rvu.edu.in>
2024-02-06 14:10:47 +0530

    test

---
+++ set02/problem03.c
@@ -0,0 +1,16 @@
+//Write a program find whether a number is a composite number
+//A Composite number has more than 2 factors.
+int input_number()
+{
+    int n;
+    printf("Enter the number:");
+    return n;
+}
+
+int is_composite(int n){
+    
+}
+
+void output(int n, int result){
+    printf("");
+}
