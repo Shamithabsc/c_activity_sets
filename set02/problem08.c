4ff004b5592b1b13ce98cd217c38ce466d5b96d3
unknown <shamithar.bsc23@rvu.edu.in>
2024-02-17 21:33:17 +0530

    test

--- set02/problem08.c
+++ set02/problem08.c
@@ -3,10 +3,30 @@
 typedef struct _triangle {
 	float base, altitude, area;
 } Triangle;
-int input_n();
 
-Triangle input_triangle();
-void input_n_triangles(int n, Triangle t[n]);
+int input_n(){
+	int n;
+	printf("Enter number of triangles:");
+	scanf("%f",&n);
+	return n;
+}
+
+Triangle input_triangle(){
+	Triangle t;
+	printf("Enter the base:");
+	scanf("%f",&t.base);
+
+	printf("Enter the altitude:");
+	scanf("%f",&t.altitude);
+	return t;
+}
+void input_n_triangles(int n, Triangle t[n])
+{
+	for(int i=0;i<n;i++){
+		printf("Enter %d no. of triangle:\n",i+1);
+		t[i]=input_triangle();
+	}
+}
 void find_area(Triangle *t);
 void find_n_areas(int n, Triangle t[n]);
 Triangle find_smallest_triangle(int n, Triangle t[n]);
