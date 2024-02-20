b61e102db093edc68ffca56e4fed1f2fd5861a1b
unknown <shamithar.bsc23@rvu.edu.in>
2024-02-20 14:43:57 +0530

    test

--- set02/problem03.c
+++ set02/problem03.c
@@ -1,16 +1,34 @@
 //Write a program find whether a number is a composite number
 //A Composite number has more than 2 factors.
+#include <stdio.h>
 int input_number()
 {
     int n;
     printf("Enter the number:");
+    scanf("%d",&n);
     return n;
 }
 
-int is_composite(int n){
-    
+int is_composite(int n)
+{
+    for(int i=2;i<=n/2;i++){
+        if(n%1==0)
+        return 1;
+    }    
+    return 0;
 }
 
 void output(int n, int result){
-    printf("");
+    if(result==1){
+        printf("%d is a composite number\n",n);
+    }
+    else{
+        printf("%d is not a composite number\n",n);
+    }
+}
+int main(){
+    int n=input_number();
+    int result=is_composite(n);
+    output(n,result);
+    return 0;
 }
--- set02/problem06.c
+++ set02/problem06.c
@@ -1,5 +1,28 @@
 //Write a program to reverse a string.
-
-void input_string(char *a);
-void str_reverse(char *str, char *rev_str);
-void output(char *a, char *reverse_a);
+#include<stdio.h>
+#include<string.h>
+void input_string(char *a)
+{
+    printf("Enter a string:");
+    scanf("%s",a);
+}
+void str_reverse(char *str, char *rev_str) {
+    int len=strlen(str);
+    for(int i=0 ; i < len ; i++){
+        rev_str[i]=str[len-i-1];
+    }
+    rev_str[len] = '\0'; // Null-terminate the reversed string
+}
+void output(char *a, char *reverse_a)
+{
+    printf("The string to be reversed is %s\n",a);
+    printf("The reversed string is:%s\n",reverse_a);
+}
+int main(){
+    char a[100];
+    char reverse[100];
+    input_string(a);
+    str_reverse(a,reverse);
+    output(a,reverse);
+    return 0;
+}
