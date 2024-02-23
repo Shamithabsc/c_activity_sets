c2fac89da2bf6e90d944aeaffd51b726ea2a6445
unknown <shamithar.bsc23@rvu.edu.in>
2024-02-23 14:06:22 +0530

    test

--- set02/problem03.c
+++ set02/problem03.c
@@ -12,7 +12,7 @@ int input_number()
 int is_composite(int n)
 {
     for(int i=2;i<=n/2;i++){
-        if(n%1==0)
+        if(n%i==0)
         return 1;
     }    
     return 0;
--- set02/problem04.c
+++ set02/problem04.c
@@ -19,7 +19,7 @@ void input_array(int n, int a[n])
 // int is_composite(int n)
 // {
 //     for(int i=2;i<=n/2;i++){
-//         if(n%1==0)
+//         if(n%i==0)
 //         return 1;
 //     }    
 //     return 0;
