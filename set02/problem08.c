4b43f80406f31d3645982f165b10b9437546e723
unknown <shamithar.bsc23@rvu.edu.in>
2024-02-23 13:44:16 +0530

    test

--- set02/problem08.c
+++ set02/problem08.c
@@ -1,5 +1,5 @@
 //Write a program to find the triangle with smallest area in n given triangles.
-
+#include<stdio.h>
 typedef struct _triangle {
 	float base, altitude, area;
 } Triangle;
@@ -7,7 +7,7 @@ typedef struct _triangle {
 int input_n(){
 	int n;
 	printf("Enter number of triangles:");
-	scanf("%f",&n);
+	scanf("%d",&n);
 	return n;
 }
 
@@ -20,31 +20,18 @@ Triangle input_triangle(){
 	scanf("%f",&t.altitude);
 	return t;
 }
-void input_n_triangles(int n, Triangle t[n])
-{
-	for(int i=0;i<n;i++){
-		printf("Enter %d no. of triangle:\n",i+1);
-		t[i]=input_triangle();
-	}
-}
-void find_area(Triangle *t)
-{
-	t->area = t->altitude * t->base * 0.5; 
-}
-void find_n_areas(int n, Triangle t[n])
-{
-	for(int i = 0; i < n; i++) {
-        find_area(&t[i]);
-     printf("The Area of %d triangle is %f\n",i+1, t[i].area);
-  }
-}
-Triangle find_smallest_triangle(int n, Triangle t[n]){
-	Triangle smallest = t[0];
-for(int i = 1; i< n; i++) {
-    find_area(&t[i]);
-    if(t[i].area < smallest.area) {
-        smallest = t[i];
-    }
-}
-}
+void input_n_triangles(int n, Triangle t[n]);
+void find_area(Triangle *t);
+void find_n_areas(int n, Triangle t[n]);
+Triangle find_smallest_triangle(int n, Triangle t[n]);
 void output(int n, Triangle t[n], Triangle smallest);
+int main(){
+	 int n;
+    n = input_n();
+    Triangle t[100], smallest;
+    input_n_triangles(n,t);
+    find_n_areas(n,t);
+    smallest = find_smallest_triangle(n,t);
+    output(n,t,smallest);
+    return 0;
+}
