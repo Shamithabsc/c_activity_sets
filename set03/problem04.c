d148cfa53d4479fc1d1327b80cbce409a19a8588
unknown <shamithar.bsc23@rvu.edu.in>
2024-02-26 13:40:54 +0530

    test

--- set03/problem04.c
+++ set03/problem04.c
@@ -1,6 +1,34 @@
 // Write a program to find nth number in fibonacci sequence.
 // Fibonacci Sequence is of the form: `0, 1, 1, 2, 3, 5, 8, 13, ...`
 #include<stdio.h>
-int input();
-int find_fibo(int n);
-void output(int n, int fibo);
+int input()
+{
+    int n;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    return n;
+}
+int find_fibo(int n)
+{
+    if (n<=1){
+        return n;
+    }
+    int t1 = 0,t2 = 1,tn;
+    for(int i = 2 ;i<=n;i++){
+    printf("the %d number:%d\n",i,t2);
+    tn = t1 + t2;
+    t1 = t2;
+    t2 = tn;
+    }
+    return t2;
+}
+void output(int n, int fibo)
+{
+    printf("fibo(%d)= %d\n",n,fibo);
+}
+int main(){
+    int n = input();
+    int fibo = find_fibo(n);
+    output(n,fibo);
+    return 0;
+}
--- set03/tempCodeRunnerFile.c
+++ set03/tempCodeRunnerFile.c
@@ -1,5 +1,3 @@
-to check the input number is not 1 since 1 isn't prime or composite.
-    if (n <= 1)
-    {
-        return 0;
+if (n<=1){
+        return n;
     }
