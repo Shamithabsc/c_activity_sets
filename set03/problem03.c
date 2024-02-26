1f249823209a991fadb4633fa450bc8791eb38e3
unknown <shamithar.bsc23@rvu.edu.in>
2024-02-26 12:00:41 +0530

    test

--- set03/problem03.c
+++ set03/problem03.c
@@ -1,5 +1,38 @@
 // Write a program to check if the given number is prime
 #include<stdio.h>
-int input_number();
-int is_prime(int n);
-void output(int n, int result);
+
+int input_number()
+{
+    int n;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    return n;
+}
+int is_prime(int n)
+{
+    //to check the input number is not 1 or <1 since negative numbers are not prime or composite.
+    if (n <= 1)
+    {   return 0;  }
+    
+    for ( int i=2; i <= n/2 ;i++)
+    {
+        if(n % i == 0){
+            return 0;
+        }
+    }
+     return 1;    
+}
+void output(int n, int result)
+{
+    if (result==1){
+    printf("%d is prime\n",n);
+    }
+    else
+    { printf("%d is not prime\n",n);   }
+}
+int main(){
+    int n =input_number();
+    int result=is_prime(n);
+    output(n,result);
+    return 0;
+}
---
+++ set03/tempCodeRunnerFile.c
@@ -0,0 +1,5 @@
+to check the input number is not 1 since 1 isn't prime or composite.
+    if (n <= 1)
+    {
+        return 0;
+    }
