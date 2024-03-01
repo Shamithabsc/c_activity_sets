4d4b37fe4e559eab612a108da3eb6854eab5917e
unknown <shamithar.bsc23@rvu.edu.in>
2024-03-01 14:11:59 +0530

    test

--- set01/problem10.c
+++ set01/problem10.c
@@ -10,11 +10,11 @@ int stringcompare(char *string1, char *string2){
     int i;
     for(i=0; string1[i]==string2[i]&&string1[i]!='\0'&&string2[i]!='\0';i++);
     if(string1[i]>string2[i])
-    return -1;
+    {return -1;}
     else if(string2[i]>string1[i])
-    return 1;
+    {return 1;}
     else 
-    return 0;    
+    {return 0;}    
 }
 void output(char *string1, char *string2, int result){
     if(result==-1)
