fcb1d8ef50398716508ab8db9d2bab42d17a162a
unknown <shamithar.bsc23@rvu.edu.in>
2024-03-06 19:13:32 +0530

    test

---
+++ set04/problem01.c
@@ -0,0 +1,8 @@
+// Write a program to find sum of two fractions
+# include<stdio.h>
+void input(int *num1, int *den1, int *num2, int *den2);
+void add(int num1, int den1, int num2, int den2, int *res_num, int *res_den);
+void output(int num1, int den1, int num2, int den2, int res_num, int res_den);
+int main(){
+    return 0;
+}
